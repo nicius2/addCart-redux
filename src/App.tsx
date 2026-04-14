3193da4200ac0e145083d816691a741420c39847
nicius2 <marco.marks06@gmail.com>
2026-04-14 09:35:56 -0300

    config: global redux structuring

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import { ThemeProvider } from "styled-components";
 import { HomePage } from "./pages/HomePage";
 import { theme } from "./styles/theme";
 import { GlobalTheme } from "./styles/global";
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "./store";
 
 export function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <GlobalTheme />
-        <HomePage />
+        <ReduxProvider store={store}>
+          <GlobalTheme />
+          <HomePage />
+        </ReduxProvider>
       </ThemeProvider>
     </>
   );
